b133837bca4dfbcbe12dec296f9a1fd511f69c93
NathanEdwardss <83931950+NathanEdwardss@users.noreply.github.com>
2021-05-10 10:30:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function oddOrEven() {
+
+  let Input = prompt("Input Number:");
+
+  if (Input % 2 == 0) {
+
+    console.log("The number is even.");
+
+  }
+
+  else {
+
+    console.log("The number is odd");
+
+  }
+
+}
+
+oddOrEven();
